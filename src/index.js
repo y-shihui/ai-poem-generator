ff288ea86b6856e79fc48620f6b2389ba4d2a55c
y-shihui <y.shihui54@gmail.com>
2025-01-12 16:29:38 +1100

    Added button function with JS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+function generatePoem(event) {
+  event.preventDefault();
+  new Typewriter("#poem", {
+    strings: "Hold fast to dreams",
+    autoStart: true,
+    delay: 100,
+  });
+}
 
+let searchSubmission = document.querySelector("#poemGenerator");
+searchSubmission.addEventListener("submit", generatePoem);
